853dbda40339d72c97e829c73d43b6c4b5f30851
Mario Souto <soutomarios@gmail.com>
2018-10-24 01:43:33 -0300

    update phone tests

--- tests/unity/domain/valueObjects/Phone.test.js
+++ tests/unity/domain/valueObjects/Phone.test.js
@@ -2,7 +2,7 @@ import { expect } from '../../setup'
 import Phone from '../../../../src/domain/valueObjects/Phone';
 
 describe('domain/valueObjects/Phone', () => {
-  it('should return pure Phone', () => {
+  it('should return pure Phone 0012345678', () => {
     const purePhoneValue = '0012345678'
     const impurePhoneValue = '00-1234-5678'
 
@@ -13,9 +13,20 @@ describe('domain/valueObjects/Phone', () => {
     expect(impurePhone.getNumber()).to.be.equal(purePhoneValue)
   })
 
-  it('should return formated Phone', () => {
+  it('should return formated Phone 00-1234-5678', () => {
     const purePhoneValue = '0012345678'
     const impurePhoneValue = '00-1234-5678'
+
+    const purePhone = new Phone({ number: purePhoneValue })
+    const impurePhone = new Phone({ number: impurePhoneValue })
+
+    expect(purePhone.getNumberFormated()).to.be.equal(impurePhoneValue)
+    expect(impurePhone.getNumberFormated()).to.be.equal(impurePhoneValue)
+  })
+
+  it('should return formated Phone 1234-5678', () => {
+    const purePhoneValue = '12345678'
+    const impurePhoneValue = '1234-5678'
 
     const purePhone = new Phone({ number: purePhoneValue })
     const impurePhone = new Phone({ number: impurePhoneValue })
