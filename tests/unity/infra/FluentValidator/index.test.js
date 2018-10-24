8d3510489356be1e045cfee492d76ab1427de671
Mario Souto <soutomarios@gmail.com>
2018-10-24 00:53:43 -0300

    update uncomment some tests

--- tests/unity/infra/FluentValidator/index.test.js
+++ tests/unity/infra/FluentValidator/index.test.js
@@ -7,32 +7,32 @@ describe('/infra/FluentValidator/ValidationContract', () => {
     contract = new ValidationContract()
   })
 
-  // describe('smoke test', () => {
-  //   it('validate() should be a method of contract', () => {
-  //     expect(contract.validate).to.be.an('function')
-  //   })
-  //   it('getErrors() should be a method of contract', () => {
-  //     expect(contract.getErrors).to.be.an('function')
-  //   })
-  //   it('hasErrors() should be a method of contract', () => {
-  //     expect(contract.hasErrors).to.be.an('function')
-  //   })
-  //   it('setParam() should be a method of contract', () => {
-  //     expect(contract.setParam).to.be.an('function')
-  //   })
-  //   it('setError() should be a method of contract', () => {
-  //     expect(contract.setError).to.be.an('function')
-  //   })
-  //   it('removeError() should be a method of contract', () => {
-  //     expect(contract.removeError).to.be.an('function')
-  //   })
-  //   it('getErrorsByParam() should be a method of contract', () => {
-  //     expect(contract.getErrorsByParam).to.be.an('function')
-  //   })
-  //   it('isValidParam() should be a method of contract', () => {
-  //     expect(contract.isValidParam).to.be.an('function')
-  //   })
-  // })
+  describe('smoke test', () => {
+    it('validate() should be a method of contract', () => {
+      expect(contract.validate).to.be.an('function')
+    })
+    it('getErrors() should be a method of contract', () => {
+      expect(contract.getErrors).to.be.an('function')
+    })
+    it('hasErrors() should be a method of contract', () => {
+      expect(contract.hasErrors).to.be.an('function')
+    })
+    it('setParam() should be a method of contract', () => {
+      expect(contract.setParam).to.be.an('function')
+    })
+    it('setError() should be a method of contract', () => {
+      expect(contract.setError).to.be.an('function')
+    })
+    it('removeError() should be a method of contract', () => {
+      expect(contract.removeError).to.be.an('function')
+    })
+    it('getErrorsByParam() should be a method of contract', () => {
+      expect(contract.getErrorsByParam).to.be.an('function')
+    })
+    it('isValidParam() should be a method of contract', () => {
+      expect(contract.isValidParam).to.be.an('function')
+    })
+  })
 
   it('should return "one" error if param name is invalid', () => {
     contract.validate('name', '').isRequired()
